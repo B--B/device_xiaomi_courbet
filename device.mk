afe4f433d1ba14732e456c7c3ca6ae31e400f28c
Marco Zanin <mrczn.bb@gmail.com>
2023-11-27 19:09:56 +0100

    Courbet: add assertive display configuration
    From MIUI V14.0.4.0.TKQMIXM
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- device.mk
+++ device.mk
@@ -33,8 +33,10 @@ PRODUCT_PACKAGES += \
 
 # Display
  PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_42_02_0b_mp_cmd_mode_dsc_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_42_02_0b_mp_cmd_mode_dsc_dsi_panel.xml \
+     $(LOCAL_PATH)/configs/display/hdr_config.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/hdr_config.cfg \
      $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_36_02_0a_mp_cmd_mode_dsc_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_36_02_0a_mp_cmd_mode_dsc_dsi_panel.xml \
+     $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_42_02_0b_mp_cmd_mode_dsc_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_42_02_0b_mp_cmd_mode_dsc_dsi_panel.xml \
+     $(LOCAL_PATH)/configs/display/sdr_config.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/sdr_config.cfg
 
 # Init scripts
 PRODUCT_COPY_FILES += \
